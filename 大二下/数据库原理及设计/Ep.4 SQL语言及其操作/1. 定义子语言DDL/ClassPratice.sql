b6d89bbb9f010635d7eac1e1890361c5dc4bde22
Super SASS <mrniconiconi@gmail.com>
2022-04-30 00:51:33 +0800

    2022.4.30 00:51

---
+++ 大二下/数据库原理及设计/Ep.4 SQL语言及其操作/1. 定义子语言DDL/ClassPratice.sql
@@ -0,0 +1,32 @@
+/* P27 */
+ALTER TABLE Student
+ADD Major varchar(20) NOT NULL
+
+ALTER TABLE Student
+ADD CONSTRAINT SName_Unique Unique(SName)
+
+ALTER TABLE Student
+ADD CONSTRAINT SGender_Check CHECK(SGender in ('F', 'M'))
+
+/* P28 */
+
+CREATE TABLE Salary
+(
+    EID char(10) Foreign key References Employee(EID),
+    SMonth char(6) Not null,
+    BSalary smallmoney,
+    PSalary smallmoney,
+    ESalary smallmoney,
+    ASalary as BSalary + PSalary + ESalary,
+    Primary key(EID, SMonth)
+)
+
+/* P43 */
+CREATE VIEW M_Track2018_SV AS
+    SELECT SID,
+        SName,
+        SGender,
+        datepart(yyyy, GetDate() - datepart(yyyy, BirthDate)),
+        Telephone
+    FROM S
+    WHERE Major = '轨道' AND EntryYear = 2018
