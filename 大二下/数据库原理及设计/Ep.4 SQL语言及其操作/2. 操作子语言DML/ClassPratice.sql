66b714ad661c068c81a6345034f7f5ceda0b2510
SuperSASS <32005046+SuperSASS@users.noreply.github.com>
2022-05-12 21:05:46 +0800

    2022.5.12 21:05

--- 大二下/数据库原理及设计/Ep.4 SQL语言及其操作/2. 操作子语言DML/ClassPratice.sql
+++ 大二下/数据库原理及设计/Ep.4 SQL语言及其操作/2. 操作子语言DML/ClassPratice.sql
@@ -23,3 +23,37 @@ DELETE SC
 DELETE SC
     FROM sc,course,teacher
     WHERE sc.cno=course.cno AND course.tno=teacher.tno AND Tname='吕燕'​
+
+/* P107 - GROUP BY */
+SELECT S.Sid, Sname, avg(Score), count(Cid) FROM S NATURAL JOIN SC GROUP BY Students.Sid, Sname​
+
+/* P112 - Practice */
+SELECT student.sno, sname, count(cno), sum(score)
+    FROM student LEFT JOIN sc /* 注意没选课的同学也要统计，因此左连接 */
+    WHERE student.sno = sc.sno
+    GROUP BY student.sno, sname
+
+SELECT count(*)
+    FROM teacher
+    WHERE tname LIKE '刘%'
+
+SELECT sno, sname
+    FROM s LEFT JOIN sc
+    GROUP BY sno, sname
+    HAVING count(cno) < (SELECT count(cno) FROM c)
+    
+SELECT cno, max(socre), min(socre)
+    FROM sc
+    GROUP BY cno
+
+SELECT c.cno, count(s.sno), sum(CASE WHEN score>60 THEN 1.0 else 0.0 END)/count(s.sno)
+    FROM c LEFT JOIN sc /* 可能存在没有学生选的课 */
+    WHERE c.cno = sc.cno
+    GROUP BY c.cno
+
+/* 有误
+SELECT s.sno, avg(score)
+    FROM s NATURAL JOIN sc
+    WHERE count(CASE WHEN score<60 THEN 1 ELSE 0 END) > 2
+    GROUP BY s.sno
+*/
